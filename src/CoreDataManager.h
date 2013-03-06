b76f99a2a9ecf818925d2cfa82eeadb59007b345
Nick McGuire <nick@nickmcguire.com>
2013-03-06 11:31:56 -0500

    Added the ability to nil a persistantStoreCoordinator
    For unit testing

--- src/CoreDataManager.h
+++ src/CoreDataManager.h
@@ -12,9 +12,9 @@
 @interface CoreDataManager : NSObject
 
 
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 @property (copy, nonatomic) NSString *databaseName;
 @property (copy, nonatomic) NSString *modelName;
 
