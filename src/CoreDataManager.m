0c732d87ba647c86f439702acc461de98487b80f
Nick McGuire <nick@nickmcguire.com>
2013-09-04 09:58:31 -0400

    Switched to not waiting to merge changes

--- src/CoreDataManager.m
+++ src/CoreDataManager.m
@@ -18,12 +18,12 @@
 static CoreDataManager *singleton;
 
 + (id)instance {
-    static dispatch_once_t singletonToken;
-    dispatch_once(&singletonToken, ^{
-        singleton = [[self alloc] init];
-    });
-    
-    return singleton;
+	static dispatch_once_t singletonToken;
+	dispatch_once(&singletonToken, ^{
+		singleton = [[self alloc] init];
+	});
+
+	return singleton;
 }
 
 - (id)init {
@@ -35,7 +35,7 @@ static CoreDataManager *singleton;
 													 name:NSManagedObjectContextDidSaveNotification
 												   object:nil];
 	}
-	
+
 	return self;
 }
 
@@ -47,13 +47,13 @@ static CoreDataManager *singleton;
 - (void)contextChanged:(NSNotification*)notification {
 	//We don't need to merge changes that we made
 	if ([notification object] == [self managedObjectContext]) return;
-	
+
 	//Make sure the work is done on the main thread
 	if (![NSThread isMainThread]) {
-		[self performSelectorOnMainThread:@selector(contextChanged:) withObject:notification waitUntilDone:YES];
+		[self performSelectorOnMainThread:@selector(contextChanged:) withObject:notification waitUntilDone:NO];
 		return;
 	}
-	
+
 	//Then merge the changes into the context
 	[[self managedObjectContext] mergeChangesFromContextDidSaveNotification:notification];
 }
@@ -62,89 +62,89 @@ static CoreDataManager *singleton;
 #pragma mark - Private
 
 - (NSString *)appName {
-    return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleName"];
+	return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleName"];
 }
 
 - (NSString *)databaseName {
-    if (_databaseName != nil) return _databaseName;
-    _databaseName = [[[self appName] stringByAppendingString:@".sqlite"] copy];
-    return _databaseName;
+	if (_databaseName != nil) return _databaseName;
+	_databaseName = [[[self appName] stringByAppendingString:@".sqlite"] copy];
+	return _databaseName;
 }
 
 - (NSString *)modelName {
-    if (_modelName != nil) return _modelName;
-    _modelName = [[self appName] copy];
-    return _modelName;
+	if (_modelName != nil) return _modelName;
+	_modelName = [[self appName] copy];
+	return _modelName;
 }
 
 #pragma mark - Public
 
 - (NSManagedObjectContext *)managedObjectContext
 {
-    if (_managedObjectContext) return _managedObjectContext;
-    
-    if (self.persistentStoreCoordinator) {
-        _managedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
-        [_managedObjectContext setPersistentStoreCoordinator:self.persistentStoreCoordinator];
-    }
-    return _managedObjectContext;
+	if (_managedObjectContext) return _managedObjectContext;
+
+	if (self.persistentStoreCoordinator) {
+		_managedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+		[_managedObjectContext setPersistentStoreCoordinator:self.persistentStoreCoordinator];
+	}
+	return _managedObjectContext;
 }
 
 - (NSManagedObjectModel *)managedObjectModel
 {
-    if (_managedObjectModel) return _managedObjectModel;
-    
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:[self modelName] withExtension:@"momd"];
-    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-    return _managedObjectModel;
+	if (_managedObjectModel) return _managedObjectModel;
+
+	NSURL *modelURL = [[NSBundle mainBundle] URLForResource:[self modelName] withExtension:@"momd"];
+	_managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+	return _managedObjectModel;
 }
 
 -(void) setUpPersistentStoreCoordinator {
-	
-    NSURL *storeURL = [self.applicationDocumentsDirectory URLByAppendingPathComponent:[self databaseName]];
-    NSError *error = nil;
-    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    
-    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
-                             [NSNumber numberWithBool:YES], NSMigratePersistentStoresAutomaticallyOption,
-                             [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
-    
-    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error])
-        NSLog(@"ERROR IN PERSISTENT STORE COORDINATOR! %@, %@", error, [error userInfo]);
+
+	NSURL *storeURL = [self.applicationDocumentsDirectory URLByAppendingPathComponent:[self databaseName]];
+	NSError *error = nil;
+	_persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+
+	NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
+							 [NSNumber numberWithBool:YES], NSMigratePersistentStoresAutomaticallyOption,
+							 [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
+
+	if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error])
+		NSLog(@"ERROR IN PERSISTENT STORE COORDINATOR! %@, %@", error, [error userInfo]);
 }
 
 - (NSPersistentStoreCoordinator *)persistentStoreCoordinator
 {
-    if (_persistentStoreCoordinator) return _persistentStoreCoordinator;
-    
-    [self setUpPersistentStoreCoordinator];
-    return _persistentStoreCoordinator;
+	if (_persistentStoreCoordinator) return _persistentStoreCoordinator;
+
+	[self setUpPersistentStoreCoordinator];
+	return _persistentStoreCoordinator;
 }
 
 
 - (BOOL)saveContext {
 	__block BOOL savedOK = NO;
-	
+
 	[self.managedObjectContext performBlockAndWait:^{
 		if (self.managedObjectContext == nil || ![self.managedObjectContext hasChanges]) return;
-		
+
 		NSError *error = nil;
-		
+
 		if (![self.managedObjectContext save:&error]) {
 			NSLog(@"Unresolved error in saving context! %@, %@", error, [error userInfo]);
 			return;
 		}
-		
+
 		savedOK = YES;
 	}];
-    
-    return savedOK;
+
+	return savedOK;
 }
 
 #pragma mark - Application's Documents directory
 - (NSURL *)applicationDocumentsDirectory {
-    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
-                                                   inDomains:NSUserDomainMask] lastObject];
+	return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
+												   inDomains:NSUserDomainMask] lastObject];
 }
 
 
